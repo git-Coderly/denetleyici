fa6f7d8218489ff423224646308ae51a6d61956b
Coderly <123462859+git-Coderly@users.noreply.github.com>
2023-03-11 08:12:42 +0300

    Add files via upload

---
+++ denetleyici.py
@@ -0,0 +1,19 @@
+import difflib
+
+# İki metni okuyun
+with open("orijinal.txt", "r") as file:
+    original_text = file.read()
+
+with open("kontrol.txt", "r") as file:
+    checked_text = file.read()
+
+# Metinleri karşılaştırın ve farkları bulun
+matcher = difflib.SequenceMatcher(None, original_text, checked_text)
+difference = matcher.ratio()
+
+# Benzerlik oranını kontrol edin
+similarity = difference * 100
+if similarity <= 70:
+    print(f"Metin özgün. * Benzerlik oranı: {similarity:.2f}%")
+else:
+    print(f"Metin özgün değil. Benzerlik oranı: {similarity:.2f}%")
